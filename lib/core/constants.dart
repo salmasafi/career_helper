96060e06b333d62de8be5218fc48d8ce6b63e1b4
Salma Safi <126074245+salmasafi@users.noreply.github.com>
2024-10-21 22:03:41 +0300

    Delete lib directory

--- lib/core/constants.dart
+++
@@ -1,7 +0,0 @@
-
-const String kLogo = 'assets/images/Logo-03.png';
-const String kMessagesCollection = 'messages';
-const String kMessage = 'message';
-const String kUser = 'user';
-const String kDate = 'date';
-
